378f9dec1eed77a44130d5ab299c5c6c656310b3
ankitahir <ahirankit2632@gmail.com>
2026-05-12 17:12:55 +0530

    changing nodemailer service

--- mail/src/consumer.ts
+++ mail/src/consumer.ts
@@ -3,14 +3,10 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 export const startSentOtpConsumer = async () => {
